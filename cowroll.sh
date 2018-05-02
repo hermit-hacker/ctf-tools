44e0c0e0e811245a5c4db024b907eb0ad7aa92d5
Hermit <bcmork@gmail.com>
2018-05-02 18:53:38 -0500

    Added cowroll (Rickroll to Cowsay to Lolcats)

---
+++ cowroll.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt-get install -y lolcat cowsay > /dev/null 2>&1
+wget -O- https://raw.githubusercontent.com/doyler/SecurityTools/master/DNSRickroll/lyrics.txt 2>/dev/null >/tmp/a.txt
+while read -r LY; do clear; echo ${LY} | /usr/games/cowsay -w | /usr/games/lolcat; sleep 1; done < /tmp/a.txt
