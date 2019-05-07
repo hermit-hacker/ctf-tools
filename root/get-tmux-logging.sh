0101a8121be59997eace408474b2f88877c2d794
Hermit <bcmork@gmail.com>
2019-05-07 00:00:24 -0400

    Create get-tmux-logging.sh

---
+++ root/get-tmux-logging.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author: Brian Mork (Hermit)
+
+# Quick script to setup Tmux Plugin Manager and Tmux-Logging
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tmux-logging ~/.tmux/plugins/tmux-logging
